7e1983dfbf1a9c3d5c82450c489aff65bf397463
Rui Lopes <rgl@ruilopes.com>
2016-12-11 10:19:25 +0000

    init

---
+++ HelloWorld/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UIKit;
+
+namespace HelloWorld
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            HelloButton.PrimaryActionTriggered += async (sender, args) =>
+            {
+                var ip = await HttpBin.GetIp();
+
+                var alert = UIAlertController.Create(
+                    "Hello?",
+                    $"Hello World!\nyour ip is {ip}",
+                    UIAlertControllerStyle.Alert);
+
+                alert.AddAction(
+                    UIAlertAction.Create(
+                        "Dismiss",
+                        UIAlertActionStyle.Default,
+                        null));
+
+                PresentViewController(alert, true, null);
+            };
+        }
+    }
+}
